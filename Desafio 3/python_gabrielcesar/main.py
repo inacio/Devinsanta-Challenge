aff93d4fe58d7d95e88472eb751db5ac820e0e19
Gabriel Cesar <gabrielcesar2@gmail.com>
2018-10-23 20:59:51 -0300

    Improvements at code in Challenge 3.

--- Desafio 3/python_gabrielcesar/main.py
+++ Desafio 3/python_gabrielcesar/main.py
@@ -3,6 +3,8 @@
 	Gabriel Cesar
 	gabrielcesar2@gmail.com
 '''
+import sys
+sys.setrecursionlimit(1000000)
 
 def factorial(i):
     if i == 0 or i == 1:
@@ -10,4 +12,11 @@ def factorial(i):
     else:
         return i * factorial(i - 1)
 
-print(factorial(5))
+j = []
+
+for i in range(1, 301):
+    j.append(i ** 2)
+
+for i in j:
+	print(factorial(i))
+
