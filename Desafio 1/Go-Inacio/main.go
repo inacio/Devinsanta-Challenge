1438a12e5e3083ca182fde4922c3a69e9ae24a9d
Inácio Régis Neto <inacioregisneto@gmail.com>
2018-10-22 22:57:06 -0300

    Create a main.go
    Solução para o Desafio 1

---
+++ Desafio 1/Go-Inacio/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := "Santa"
+	b := "rem"
+  	cont := 1
+    	for cont  <= 200 {
+          if cont % 5 == 0 && cont % 6 == 0 {
+            fmt.Println(cont,a,b)
+          } else if cont  % 6 == 0 {
+            fmt.Println(cont ," ",b)
+          }else if cont  % 5 == 0 {
+            fmt.Println(cont ," ",a)
+          }else{
+            fmt.Println(cont)
+          }
+
+          cont  = cont + 1
+    	}
+}
+
+
+//teste: $ go run main.go
