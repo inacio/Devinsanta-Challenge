212bd5d64fe00074f3f92ff65c3208ebea97ae91
Inácio Régis Neto <inacioregisneto@gmail.com>
2018-10-26 03:30:34 -0300

    Melhoria na solução do desafio 3 em Go lang
    utilizando a função MulRange do bigInt o algoritmo melhorou o desempenho. Ao imprimir o array apenas no final, o Go executou em 2.26min. Já com o print dentro do for apenas, o mesmo levou 36s.

--- Desafio 3/Go_Inacio/desafio3.go
+++ Desafio 3/Go_Inacio/desafio3.go
@@ -1,5 +1,4 @@
 //Esta solução não está com bom desempenho, gostaria que ajudassem a melhora-la se possivel
-
 package main
 
 import (
@@ -9,22 +8,6 @@ import (
 	"time"
 )
 
-func factorial(n *big.Int) (result *big.Int) {
-	//fmt.Println("n = ", n)
-	result = new(big.Int)
-
-	switch n.Cmp(&big.Int{}) {
-	case -1, 0:
-		result.SetInt64(1)
-	default:
-		result.Set(n)
-		var one big.Int
-		one.SetInt64(1)
-		result.Mul(result, factorial(n.Sub(n, &one)))
-	}
-	return
-}
-
 func main() {
 	start := time.Now()
 	limite := int64(299)
@@ -32,18 +15,20 @@ func main() {
 
 	cont := int64(2)
 	i := int64(0)
-	n := big.NewInt(2)
+	//n := big.NewInt(2)
+	var f big.Int
 	for i < limite {
-		fmt.Print(n, " ")
-		//go func() { //travou meu computador ao utilizar o goroutines, sei que iria embaralhar mas gostaria de ver como ficaria o desempenho (usou 100% de todos os meus núcleos) :(
-		array[i] = factorial(n.Mul(n, n))
+		//fmt.Print(cont, " ")
+		//go func() { //
+		array[i] = f.MulRange(int64(1), int64(cont*cont))
+		//fmt.Println(&f)//apenas com este print 36s para finalizar
 		cont++
-		n = big.NewInt(int64(cont))
+		//n = big.NewInt(int64(cont))
 		i++
 		//}()
 	}
 
-	fmt.Println(array)
+	fmt.Println(&array)//2.26min para finalizar
 
 	elapsed := time.Since(start)
 	log.Printf("\n\n\n Tempo para executar: %s", elapsed)
